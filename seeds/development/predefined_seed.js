84c100948fc54bbce7eb0feadef8f557f74c588a
=lykmapipo <=lallyelias87@gmail.com>
2017-01-12 14:28:03 +0300

    clear predefineds

--- seeds/development/predefined_seed.js
+++ seeds/development/predefined_seed.js
@@ -8,28 +8,22 @@
  * @param  {Function} done [description]
  * @return {[type]}        [description]
  */
-module.exports = [{
-  name: 'name',
-  value: 'Sample Company',
-  default: true
-}, {
-  name: 'email',
-  value: 'info@sample.com',
-  default: true
-}, {
-  name: 'phoneNumber',
-  value: '255714095061',
-  default: true
-}, {
-  name: 'dateFormat',
-  value: 'dd/MM/yyyy',
-  default: true
-}, {
-  name: 'timeFormat',
-  value: 'hh:mm:ss',
-  default: true
-}, {
-  name: 'defaultPassword',
-  value: 'guest',
-  default: true
-}];
+module.exports = [
+  //settings
+  {
+    name: 'dateFormat',
+    value: 'dd/MM/yyyy',
+    default: true
+  }, {
+    name: 'timeFormat',
+    value: 'hh:mm:ss',
+    default: true
+  }, {
+    name: 'password',
+    value: 'guest',
+    default: true
+  }
+
+  //statuses
+  //priorities
+];
