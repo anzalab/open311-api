ab61a17ab6e270e23629499fb0515aa8c25dc957
=lykmapipo <=lallyelias87@gmail.com>
2017-01-25 21:57:22 +0300

    commit all local changes

--- app/routers/service_request_router.js
+++ app/routers/service_request_router.js
@@ -20,70 +20,70 @@ const controller =
 const jwtAuth = require(path.join(__dirname, '..', 'middlewares', 'jwtAuth'));
 
 //add specific middlewares to servicerequests router
-router.all('/issues*', jwtAuth);
+router.all('/servicerequests*', jwtAuth);
 
 /**
- * Handle Http GET on /issues
+ * Handle Http GET on /servicerequests
  * @description display a list of all servicerequests
  * @param  {HttpRequest} request  a http request
  * @param  {HttpResponse} response a http response
  */
-router.get('/issues', function (request, response, next) {
+router.get('/servicerequests', function (request, response, next) {
   controller.index(request, response, next);
 });
 
 
 /**
- * Handle Http POST on /issues
+ * Handle Http POST on /servicerequests
  * @description create a new servicerequest
  * @param  {HttpRequest} request  a http request
  * @param  {HttpResponse} response a http response
  */
-router.post('/issues', function (request, response, next) {
+router.post('/servicerequests', function (request, response, next) {
   controller.create(request, response, next);
 });
 
 
 /**
- * Handle Http GET on /issues/:id
+ * Handle Http GET on /servicerequests/:id
  * @description display a specific servicerequest
  * @param  {HttpRequest} request  a http request
  * @param  {HttpResponse} response a http response
  */
-router.get('/issues/:id', function (request, response, next) {
+router.get('/servicerequests/:id', function (request, response, next) {
   controller.show(request, response, next);
 });
 
 
 /**
- * Handle Http PUT on /issues/:id
+ * Handle Http PUT on /servicerequests/:id
  * @description update a specific servicerequest
  * @param  {HttpRequest} request  a http request
  * @param  {HttpResponse} response a http response
  */
-router.put('/issues/:id', function (request, response, next) {
+router.put('/servicerequests/:id', function (request, response, next) {
   controller.update(request, response, next);
 });
 
 
 /**
- * Handle Http PATCH on /issues/:id
+ * Handle Http PATCH on /servicerequests/:id
  * @description update a specific servicerequest
  * @param  {HttpRequest} request  a http request
  * @param  {HttpResponse} response a http response
  */
-router.patch('/issues/:id', function (request, response, next) {
+router.patch('/servicerequests/:id', function (request, response, next) {
   controller.update(request, response, next);
 });
 
 
 /**
- * Handle Http DELETE on /issues/:id
+ * Handle Http DELETE on /servicerequests/:id
  * @description delete a specific servicerequest
  * @param  {HttpRequest} request  a http request
  * @param  {HttpResponse} response a http response
  */
-router.delete('/issues/:id', function (request, response, next) {
+router.delete('/servicerequests/:id', function (request, response, next) {
   controller.destroy(request, response, next);
 });
 
