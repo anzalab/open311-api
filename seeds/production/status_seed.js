590ce8b82fae410ebcfff78116ada0b32d4bebae
lykmapipo <lallyelias87@gmail.com>
2017-07-27 17:24:52 +0300

    add additional statuses

--- seeds/production/status_seed.js
+++ seeds/production/status_seed.js
@@ -8,16 +8,24 @@
  * @description seed statuses
  */
 const statuses = [{
+  name: 'Dropped',
+  weight: -20,
+  color: '#1A237E' //material indigo-900
+}, {
   name: 'Open',
   weight: -5,
   color: '#0D47A1' //material blue-900
 }, {
-  name: 'In Progress',
+  name: 'Escalated',
   weight: 0,
+  color: '#FF6F00' //material amber-900
+}, {
+  name: 'In Progress',
+  weight: 5,
   color: '#F9A825' //material yellow-800
 }, {
   name: 'Closed',
-  weight: 5,
+  weight: 10,
   color: '#1B5E20' //material green-900
 }];
 
