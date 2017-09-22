ae707742625a1736bbeae302bd864fd9d22cff44
lykmapipo <lallyelias87@gmail.com>
2017-09-22 20:00:44 +0300

    update contact method to use ContactMethod schema to support workspace used to receive or attend a service request

--- app/models/service_request_model.js
+++ app/models/service_request_model.js
@@ -68,6 +68,54 @@ const CONTACT_METHODS = [
 
 
 /**
+ * @name ContactMethod
+ * @description schema for representing a method used by reporter and workspace
+ *              used to receive the service request. Example a customer may
+ *              call call center and operator log the service request, then
+ *              a contact method is a call and workspace is call center.
+ * @type {Schema}
+ * @since 0.1.0
+ * @version 0.1.0
+ * @private
+ */
+const ContactMethod = new Schema({
+  /**
+   * @name name
+   * @description A communication(contact) method(mechanism) used by a reporter
+   *              to report the issue
+   *
+   * @type {Object}
+   * @private
+   * @since 0.1.0
+   * @version 0.1.0
+   */
+  name: {
+    type: String,
+    index: true,
+    default: CONTACT_METHOD_PHONE_CALL,
+    enum: CONTACT_METHODS,
+    searchable: true
+  },
+
+
+  /**
+   * @name workspace
+   * @description workspace used be operator to receive service request
+   * @type {Object}
+   * @private
+   * @since 0.1.0
+   * @version 0.1.0
+   */
+  workspace: {
+    type: String,
+    index: true,
+    searchable: true
+  }
+
+}, { _id: false, id: false, timestamps: false });
+
+
+/**
  * @name ServiceRequestSchema
  * @type {Schema}
  * @since 0.1.0
@@ -293,9 +341,7 @@ const ServiceRequestSchema = new Schema({
    * @version 0.1.0
    */
   method: {
-    type: String,
-    enum: CONTACT_METHODS,
-    default: CONTACT_METHOD_PHONE_CALL,
+    type: ContactMethod,
     index: true,
     searchable: true
   },
