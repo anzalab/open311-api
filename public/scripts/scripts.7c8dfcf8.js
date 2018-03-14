ef258b0ba8faca8e1c543c60953f15f3bc559de5
lykmapipo <lallyelias87@gmail.com>
2018-03-14 17:40:15 +0300

    merge mwauwasa ui build

--- public/scripts/scripts.7c8dfcf8.js
+++ public/scripts/scripts.7c8dfcf8.js
@@ -349,7 +349,7 @@ angular
 
 angular.module('ng311')
 
-.constant('ENV', {name:'production',owner:'DAWASCO',title:'SmartDawasco',version:'v0.1.0',description:'Citizen Feedback System',apiEndPoint:{mobile:'',web:''},socketEndPoint:{mobile:'',web:''},socketEnable:false,settings:{locale:'sw',name:'open311',email:'example@example.com',phone:'(000) 000 000 000',currency:'USD',dateFormat:'dd/MM/yyyy',timeFormat:'hh:mm:ss',defaultPassword:'guest',abbreviations:{thousand:'K',million:'M',billion:'B',trillion:'T'}}})
+.constant('ENV', {name:'production',owner:'MWAUWASA',title:'MWAUWASA',version:'v0.1.0',description:'Citizen Feedback System',apiEndPoint:{mobile:'',web:''},socketEndPoint:{mobile:'',web:''},socketEnable:false,settings:{locale:'sw',name:'open311',email:'example@example.com',phone:'(000) 000 000 000',currency:'USD',dateFormat:'dd/MM/yyyy',timeFormat:'hh:mm:ss',defaultPassword:'guest',abbreviations:{thousand:'K',million:'M',billion:'B',trillion:'T'}}})
 
 ;
 'use strict';
