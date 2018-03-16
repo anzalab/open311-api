2e8caac3476e0262cdb7b23200c23180e9864d90
lykmapipo <lallyelias87@gmail.com>
2018-03-16 11:40:15 +0300

    restore jurisdiction middlewares

--- app/middlewares/jurisdiction.js
+++ app/middlewares/jurisdiction.js
@@ -4,58 +4,48 @@
 const _ = require('lodash');
 
 /**
- * @name jurisdiction
- * @description middleware to extend request with jurisdiction(s) filters
- * @param  {HttpRequest}   request  http request
- * @param  {HttpResponse}   response http response
- * @param  {Function} next     next http middleware to be invoked
- */
+* @name jurisdiction
+* @description middleware to extend request with jurisdiction(s) filters
+* @param  {HttpRequest}   request  http request
+* @param  {HttpResponse}   response http response
+* @param  {Function} next     next http middleware to be invoked
+*/
 module.exports = function (request, response, next) {
 
-  if (request && request.party) {
+ if (request && request.party) {
 
-    //obtain request query criteria
-    const criteria = _.get(request, 'mquery.query', {});
+   //obtain request query criteria
+   const criteria = _.get(request, 'mquery.query', {});
 
-    //ensure filtered by jurisdiction
-    if (!criteria.jurisdiction) {
+   //ensure filtered by jurisdiction
+   if (!criteria.jurisdiction) {
 
-      //obtain party jurisdictions
-      request.party.jurisdictions(function (error, jurisdictions) {
+     //obtain party jurisdictions
+     request.party.jurisdictions(function (error, jurisdictions) {
 
-        if (!error && !_.isEmpty(jurisdictions)) {
-          jurisdictions = _.map(jurisdictions, '_id');
-          // criteria.jurisdiction = { $in: jurisdictions };
-          criteria.jurisdiction = {
-            $or: [{
-              jurisdiction: {
-                $in: jurisdictions
-              }
-            }, {
-              jurisdiction: { $eq: null }
-            }]
-          };
+       if (!error && !_.isEmpty(jurisdictions)) {
+         jurisdictions = _.map(jurisdictions, '_id');
+         criteria.jurisdiction = { $in: jurisdictions };
+       }
 
-        }
+       //restore criteria
+       request.mquery.query = criteria;
+       next();
 
-        //restore criteria
-        request.mquery.query = criteria;
-        next();
+     });
 
-      });
+   }
 
-    }
+   //continue
+   else {
+     next();
+   }
 
-    //continue
-    else {
-      next();
-    }
+ }
 
-  }
-
-  //continue
-  else {
-    next();
-  }
+ //continue
+ else {
+   next();
+ }
 
 };
