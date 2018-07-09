a8fb93dbe38d4c36122bdf20620871aa919dcf63
lykmapipo <lallyelias87@gmail.com>
2018-07-09 11:46:50 +0300

    merge latest build

--- public/scripts/scripts.ea1474e6.js
+++ public/scripts/scripts.ea1474e6.js
@@ -347,7 +347,7 @@ angular
 
 angular.module('ng311')
 
-.constant('ENV', {name:'production',owner:'MMAUWASA',author:'MMAUWASA',title:'MMAUWASA',version:'v0.1.0',description:'Citizen Feedback System',apiEndPoint:{web:'',mobile:''},socketEndPoint:{web:'',mobile:''},socketEnable:false,settings:{locale:'sw',name:'MMAUWASA',email:'lallyelias87@gmail.com',phone:'(000) 000 000 000',currency:'USD',dateFormat:'dd/MM/yyyy',timeFormat:'hh:mm:ss',defaultPassword:'guest',abbreviations:{thousand:'K',million:'M',billion:'B',trillion:'T'}}})
+.constant('ENV', {name:'production',owner:'MWAUWASA',author:'MWAUWASA',title:'MWAUWASA',version:'v0.1.0',description:'Citizen Feedback System',apiEndPoint:{web:'',mobile:''},socketEndPoint:{web:'',mobile:''},socketEnable:false,settings:{locale:'sw',name:'MWAUWASA',email:'lallyelias87@gmail.com',phone:'(000) 000 000 000',currency:'USD',dateFormat:'dd/MM/yyyy',timeFormat:'hh:mm:ss',defaultPassword:'guest',abbreviations:{thousand:'K',million:'M',billion:'B',trillion:'T'}}})
 
 ;
 'use strict';
