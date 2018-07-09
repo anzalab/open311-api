244c2cdf0d8cf881885ed39a489361d197284a59
lykmapipo <lallyelias87@gmail.com>
2018-07-09 11:00:23 +0300

    merge latest ui build

--- public/scripts/scripts.7072912e.js
+++ public/scripts/scripts.7072912e.js
@@ -347,7 +347,7 @@ angular
 
 angular.module('ng311')
 
-.constant('ENV', {name:'development',owner:'MajiFix',author:'MajiFix',title:'MajiFix',version:'v0.1.0',description:'Citizen Feedback System',apiEndPoint:{web:'',mobile:''},socketEndPoint:{web:'',mobile:''},socketEnable:false,settings:{locale:'sw',name:'MajiFix',email:'lallyelias87@gmail.com',phone:'(000) 000 000 000',currency:'USD',dateFormat:'dd/MM/yyyy',timeFormat:'hh:mm:ss',defaultPassword:'guest',abbreviations:{thousand:'K',million:'M',billion:'B',trillion:'T'}}})
+.constant('ENV', {name:'production',owner:'MMAUWASA',author:'MMAUWASA',title:'MMAUWASA',version:'v0.1.0',description:'Citizen Feedback System',apiEndPoint:{web:'',mobile:''},socketEndPoint:{web:'',mobile:''},socketEnable:false,settings:{locale:'sw',name:'MMAUWASA',email:'lallyelias87@gmail.com',phone:'(000) 000 000 000',currency:'USD',dateFormat:'dd/MM/yyyy',timeFormat:'hh:mm:ss',defaultPassword:'guest',abbreviations:{thousand:'K',million:'M',billion:'B',trillion:'T'}}})
 
 ;
 'use strict';
