8a8f3119b016cca623d2d70e5078982d983a4034
lykmapipo <lallyelias87@gmail.com>
2018-08-14 19:56:42 +0300

    update service controller to support legacy api

--- app/models/service_model.js
+++ app/models/service_model.js
@@ -18,6 +18,24 @@
 const { Service } = require('@codetanzania/majifix-service');
 
 
+//static utility to map service to legacy service structure
+Service.mapToLegacy = function mapToLegacy(service) {
+  const _service = service.toObject();
+  _service.name = service.name.en;
+  _service.description = service.description.en;
+  _service.isExternal = (service.flags || {}).external;
+  if (service.group) {
+    _service.group.name = service.group.name.en;
+    _service.group.description = (service.group.description || {}).en;
+  }
+  if (service.priority) {
+    _service.priority.name = service.priority.name.en;
+    _service.priority.description = (service.priority.description || {}).en;
+  }
+  return _service;
+}
+
+
 /**
  * @name Service
  * @description register ServiceSchema and initialize Service
