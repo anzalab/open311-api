934517f148951c8ec37c96aa3f6d790205ad2fbc
lykmapipo <lallyelias87@gmail.com>
2018-09-16 13:48:48 +0300

    upgrade to use postman for message sending

--- worker.js
+++ worker.js
@@ -5,36 +5,18 @@
  * @description job worker process for open311 API
  *
  * Alert!: Please ensure your have installed Redis Server
- * 
+ *
  * Alert!: Run worker in the separate process from main process
  *
  * Recommendation!: It also adviced to run each worker per process or per machine
- * with any number of concurrency(10 is sufficient max worker so far) 
+ * with any number of concurrency(10 is sufficient max worker so far)
  */
 
 
-//set environment to development by default
-if (!(process.env || {}).NODE_ENV) {
-  process.env.NODE_ENV = 'development';
-}
-
-
-//suppress configuration warning
-process.env.SUPPRESS_NO_CONFIG_WARNING = true;
-
-
-//dependencies
+/* dependencies */
 const path = require('path');
-const config = require('config'); //load configurations
-const environment = require('execution-environment');
+const { worker, httpServer } = require('@lykmapipo/postman');
 const mkdir = require('mkdir-p');
-const kue = require('kue');
-
-
-//register environment variables
-environment.registerEnvironments({
-  isLocal: ['test', 'dev', 'development']
-});
 
 
 //build logs directory if does not exists
@@ -42,37 +24,13 @@ const logPath = path.join(__dirname, 'logs');
 mkdir.sync(logPath);
 
 
-//setup application mongoose instance
+/* setup models */
 require(path.join(__dirname, 'app', 'initializers', 'mongoose'));
 
-//------------------------------------------------------------------------
-// Transports Initialization & Run
-//------------------------------------------------------------------------
 
-//initialize infobip sms transport
-const infobip = require('open311-infobip');
-let infobipOptions = config.get('infobip');
-if (process.env.REDIS_URL) {
-  infobipOptions.redis = process.env.REDIS_URL;
-}
-infobip.options = infobipOptions;
+/* run work */
+worker.start();
 
 
-//start
-infobip.start();
-
-
-//initialize mongoose-kue to run schema methods in background
-const worker = require('mongoose-kue').worker;
-const mongoose = require('mongoose');
-let mongooseKueOptions = { mongoose: mongoose };
-if (process.env.REDIS_URL) {
-  mongooseKueOptions.redis = process.env.REDIS_URL;
-}
-worker.start(mongooseKueOptions);
-
-
-//open web interface to monitor jobs
-if (!process.env.REDIS_URL) {
-  kue.app.listen(9000);
-}
+/* open web interface to monitor jobs */
+httpServer.listen(9000);
