1cee12714eefaeed39c74d7afe38ffea8931fa8c
lykmapipo <lallyelias87@gmail.com>
2019-06-28 15:40:58 +0300

    refactor: use latest dependencies api

--- app/initializers/mongoose.js
+++ app/initializers/mongoose.js
@@ -3,9 +3,8 @@
 
 /* dependencies */
 const path = require('path');
-const { getString } = require('@lykmapipo/env');
+const { connect } = require('@lykmapipo/mongoose-common');
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 // mongoose.set('debug', true);
 const pluginPath = path.join(__dirname, '..', 'libs', 'mongoose');
 const mongooseShow = require(path.join(pluginPath, 'show'));
@@ -14,9 +13,6 @@ const mongooseList = require(path.join(pluginPath, 'list'));
 const mongooseReload = require(path.join(pluginPath, 'reload'));
 const mongooseSoftDelete = require(path.join(pluginPath, 'soft_delete'));
 const { createModels } = require('@lykmapipo/file');
-
-/* ensure mongodb url */
-const MONGODB_URI = getString('MONGODB_URI', 'mongodb://localhost/open311');
 
 
 /* plugin global schema plugin to allow virtuals in toJSON */
@@ -50,17 +46,9 @@ require('require-all')({
 
 
 /* establish mongodb connection */
-const isConnected = (
-  mongoose.connection &&
-  mongoose.connection.readyState !== mongoose.STATES.disconnected
-);
-if (!isConnected) {
-  mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-  mongoose.connection.once('open', function () {
-    // initialize common file models
-    createModels();
-  });
-}
+connect(function () {
+  createModels(); // initialize common file models
+});
 
 /**
  * @description export mongoose
