93e899201dc1a5c2febe3313167c34d7a4a18eac
Benson Maruchu <benmaruchu@gmail.com>
2019-09-13 11:22:42 +0300

    refactor: update majifix-analytics to v0.5.0
    This use newly exposed analyticRouter

--- app/application.js
+++ app/application.js
@@ -1,5 +1,4 @@
-'use strict';
-
+"use strict";
 
 /**
  * @module application
@@ -26,63 +25,55 @@
  * @public
  */
 
-
 /* dependencies */
-const path = require('path');
-const { getString } = require('@lykmapipo/env');
-const { app, mount } = require('@lykmapipo/express-common');
-const mkdir = require('mkdir-p');
-const respond = require('express-respond');
+const path = require("path");
+const { getString } = require("@lykmapipo/env");
+const { app, mount } = require("@lykmapipo/express-common");
+const mkdir = require("mkdir-p");
+const respond = require("express-respond");
 
 /* constants */
-const BASE_PATH = getString('BASE_PATH', process.cwd());
-const LOG_PATH = getString('LOG_PATH', path.join(BASE_PATH, 'logs'));
-
+const BASE_PATH = getString("BASE_PATH", process.cwd());
+const LOG_PATH = getString("LOG_PATH", path.join(BASE_PATH, "logs"));
 
 // build logs directory if does not exists
 mkdir.sync(LOG_PATH);
 
-
 //setup application mongoose instance
-require(path.join(__dirname, 'initializers', 'mongoose'));
-
+require(path.join(__dirname, "initializers", "mongoose"));
 
 //use express respond to force
 //response content type to json always
 app.use(respond);
 
-
 //bind settings loader middleware(TODO: cleanup)
-app.use(require(path.join(__dirname, 'middlewares', 'settings')));
-app.use(require(path.join(__dirname, 'middlewares', 'preloader')));
-app.use(require(path.join(__dirname, 'middlewares', 'defaults')));
-
+app.use(require(path.join(__dirname, "middlewares", "settings")));
+app.use(require(path.join(__dirname, "middlewares", "preloader")));
+app.use(require(path.join(__dirname, "middlewares", "defaults")));
 
 /* load legacy routers recursively */
-require('require-all')({
-  dirname: path.join(__dirname, 'routers'),
+require("require-all")({
+  dirname: path.join(__dirname, "routers"),
   filter: /(.+_router)\.js$/,
   excludeDirs: /^\.(git|svn|md)$/,
-  resolve: function (router) {
+  resolve: function(router) {
     app.use(router);
   }
 });
 
-
 /* load majifix modules versioned routers */
-mount(require('@lykmapipo/permission').permissionRouter);
-mount(require('@lykmapipo/role').roleRouter);
-mount(require('@lykmapipo/predefine').predefineRouter);
-mount(require('@lykmapipo/file').fileRouter);
-mount(require('@codetanzania/majifix-jurisdiction').router);
-mount(require('@codetanzania/majifix-priority').router);
-mount(require('@codetanzania/majifix-status').router);
-mount(require('@codetanzania/majifix-service-group').router);
-mount(require('@codetanzania/majifix-service').router);
-mount(require('@codetanzania/majifix-account').router);
-mount(require('@codetanzania/majifix-alert').router);
-mount(require('@codetanzania/majifix-analytics').router);
-
+mount(require("@lykmapipo/permission").permissionRouter);
+mount(require("@lykmapipo/role").roleRouter);
+mount(require("@lykmapipo/predefine").predefineRouter);
+mount(require("@lykmapipo/file").fileRouter);
+mount(require("@codetanzania/majifix-jurisdiction").router);
+mount(require("@codetanzania/majifix-priority").router);
+mount(require("@codetanzania/majifix-status").router);
+mount(require("@codetanzania/majifix-service-group").router);
+mount(require("@codetanzania/majifix-service").router);
+mount(require("@codetanzania/majifix-account").router);
+mount(require("@codetanzania/majifix-alert").router);
+mount(require("@codetanzania/majifix-analytics").analyticRouter);
 
 /* export express application */
 exports = module.exports = app;
