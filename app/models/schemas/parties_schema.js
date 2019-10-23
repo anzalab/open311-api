c005f9dbbf3cd126a6e9fa90edc16f324465af73
lykmapipo <lallyelias87@gmail.com>
2019-10-23 08:56:17 +0300

    feat(servicerequest): add team field

--- app/models/schemas/parties_schema.js
+++ app/models/schemas/parties_schema.js
@@ -6,6 +6,7 @@ const Party = require('../party_model');
 let operator;
 let assignee;
 let changer;
+let team;
 
 /**
  * @name parties
@@ -99,6 +100,33 @@ exports.changer = changer = {
   aggregatable: { unwind: true }
 };
 
+/**
+ * @name team
+ * @description A parties who work, follow and watch a service request(issue).
+ *
+ * These are parties get notified when an action(or changes) has been applied
+ * to a service request.
+ *
+ * Assignee and Operator are directly subscribed to a team, other parties may
+ * opt to follow or added.
+ *
+ * @type {Object}
+ * @see {@link Party}
+ * @since 0.1.0
+ * @version 0.1.0
+ * @instance
+ */
+exports.team = team = {
+  type: [ObjectId],
+  ref: Party.MODEL_NAME,
+  index: true,
+  // exists: true,
+  autopopulate: {
+    select: 'name email phone',
+    maxDepth: 1
+  },
+};
 
-exports.requestParties = { operator, assignee };
+
+exports.requestParties = { operator, assignee, team };
 exports.changelogParties = { assignee, changer };
